dad6894c06ea1915cc5d947dbe7c4b10e6921e8e
Heath Anderson <heath@heathanderson.net>
2016-09-30 14:41:21 -0400

    Add styling helper gems recipe
    Why?
    
    > We want the gems for:
    >
    > * bourbon
    > * bitters
    > * neat
    >
    > to have a recipe.
    closes https://github.com/spartansystems/spartan-composer-recipes/issues/3
    
    How?
    
    * add recipe that changes `app/assets/sytlesheets.css` to `.scss`
    * add recipe for bourbon that includes the gem and imports bourbon
      into the scss file
    * add recipe for bitters that includes the gem, runs the generator,
      and imports it into the scss file
    * add recipe for neat that includes the gem

---
+++ recipes/bitters.rb
@@ -0,0 +1,17 @@
+gem 'bitters'
+
+stage_two do
+  run 'cd app/assets/stylesheets/ && bundle exec bitters install'
+  insert_into_file 'app/assets/stylesheets/application.scss',
+                   "@import \"base/base\";\n",
+                   after: "@import \"bourbon\";\n"
+end
+
+__END__
+
+name: bitters
+description: "Add bitters to your application"
+
+category: frontend
+requires: [bourbon]
+run_after: [bourbon]
---
+++ recipes/bourbon.rb
@@ -0,0 +1,14 @@
+gem 'bourbon'
+
+stage_two do
+  prepend_to_file 'app/assets/stylesheets/application.scss',
+                  "@import \"bourbon\";\n"
+end
+__END__
+
+name: bourbon
+description: "Add bourbon to your application"
+
+category: frontend
+requires: [sass_setup]
+run_after: [sass_setup]
