172431ab2f706e04277e93991e54e97d0d2f601d
Heath Anderson <heath@heathanderson.net>
2016-10-14 11:14:23 -0400

    Add required files
    Why?
    
    Some recipes require files, and since we are using `copy_from_repo`
    these files need to be in the repo before the recipes will work.
    
    The recipes that use these files will be included in a future PR.
    
    How?
    
    * include needed files
    * rename `config/` to `files/` to be more generic
    * update the slim_lint recipe to reference the new location

--- recipes/slim_lint.rb
+++ recipes/slim_lint.rb
@@ -1,8 +1,8 @@
-REPO = 'https://raw.githubusercontent.com/spartansystems/spartan-composer-recipes/master/configs/'.freeze
-gem 'slim_lint'
+REPO = "https://raw.githubusercontent.com/spartansystems/spartan-composer-recipes/master/files/".freeze
+gem "slim_lint"
 
 after_bundle do
-  copy_from_repo '.slim-lint.yml', repo: REPO
+  copy_from_repo ".slim-lint.yml", repo: REPO
 end
 
 __END__
