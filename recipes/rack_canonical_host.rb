a8a353727f2118754f63fc111c8087554565400c
Jacob <realshin@gmail.com>
2016-10-28 13:14:11 -0400

    Add new APPLICATION_HOST paradigm
    Why:
    
    * We kinda need APPLICATION_HOST everywhere now
    
    This change addresses the need by:
    
    * Adding the config to the application.rb
    * Adding config value to env files
    * Turning on SSL

--- recipes/rack_canonical_host.rb
+++ recipes/rack_canonical_host.rb
@@ -2,7 +2,8 @@ gem "rack-canonical-host"
 
 middleware_setup = <<-TEXT
 
-  config.middleware.insert_after 0, Rack::CanonicalHost, ENV['APPLICATION_HOST'] if ENV.key?('APPLICATION_HOST')
+  config.application_host = ENV.fetch('APPLICATION_HOST')
+  config.middleware.insert_after 0, Rack::CanonicalHost, config.application_host if Rails.env.production?
 TEXT
 
 inject_into_file(
@@ -10,6 +11,30 @@ inject_into_file(
   middleware_setup,
   after: "class Application < Rails::Application"
 )
+
+uncomment_lines(
+  "config/environments/production.rb",
+  "config.force_ssl = true"
+)
+
+inject_into_file(
+  "config/environments/production.rb",
+  "\n config.ssl_options = { host: config.application_host }",
+  after: "config.force_ssl = true"
+)
+
+mailer_host = <<-TEXT
+
+  config.action_mailer.default_url_options = { host: config.application_host }
+TEXT
+
+%w(production development).each do |env|
+  inject_into_file(
+    "config/environments/#{env}.rb",
+    mailer_host,
+    after: "config.action_mailer.raise_delivery_errors = false"
+  )
+end
 
 __END__
 
